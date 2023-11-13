405bb7068939c8be18e15373020788ab38662005
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2023-11-13 10:54:35 +0000

    a

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DiscordDMBlocker
 自動でセキュリティ措置を有効化し続けるBot
+ 
 製作中
 ### 参考
 [SpecialAgency-Chat/NoMoreDM](https://github.com/SpecialAgency-Chat/NoMoreDM/tree/main)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const cron = require("node-cron");
 const fetch = require("@replit/node-fetch");
 const Database = require("better-sqlite3-multiple-ciphers");
 const fs = require("fs");
+const path = require("path");
 
 const db = new Database("./.data/data.db");
 
@@ -12,8 +13,32 @@ db.exec(fs.readFileSync("./init.sql").toString());
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.token = process.env.token;
+client.commands = new Collection();
+const cmd = [];
 
-client.on("ready", () => {
+const cmdpath = path.join(__dirname,"cmd");
+const cmdfiles = fs.readdirSync(cmdpath).filter(file => file.endsWith('.js'));
+for(const file of cmdfiles){
+    const filePath = path.join(cmdpath, file);
+    require.cache[require.resolve(filePath)] = undefined;
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+        cmd.push(command.data)
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+
+
+client.on("ready", async() => {
+    try {
+        await client.application.commands.set(cmd);
+    }
+    catch (e) {
+        console.error(e)
+    }
     console.log(`Ready to as ${client.user.tag}`);
 });
 
@@ -31,12 +56,22 @@ cron.schedule("0 0,12 * * *", async () => {
     }
 });
 
-client.on("guildDelete",async(guild)=>{
+client.on("guildDelete", async (guild) => {
     db.prepare("delete from guilds where id = ?").run(guild.id);
 });
 
-client.on("guildCreate",async(guild)=>{
-    db.prepare("insert into guilds values( ? , ? , ? )").run(guild.id,"false","false");
+client.on("guildCreate", async (guild) => {
+    db.prepare("insert into guilds values( ? , ? , ? )").run(guild.id, "false", "false");
+});
+
+client.on("interactionCreate", async (int) => {
+    const command = client.commands.get(int.commandName);
+    if (!command) return;
+    try {
+        await command.execute(int, client, db);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 client.login();
